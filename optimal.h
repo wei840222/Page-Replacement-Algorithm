3c8962923903de6c0724f063088d1f2a1791eaee
0penth3wind0w <0penth3wind0w@gmail.com>
2017-12-26 17:47:35 +0800

    add utOptimal.cpp

--- optimal.h
+++ optimal.h
@@ -9,14 +9,30 @@ public:
   Optimal(string s = ""){}
   string victimPage() const
   {
-    
+    string ret = "";
+    if (isPageFault()){
+      ret += to_string(_optimalIndex) + ", ";
+      if (_frameStatus.empty() || _frameStatus.size() < _optimalIndex || _frameStatus[_optimalIndex] == '\0')
+        ret += "NULL";
+      else{
+        //
+      }
+      ret += " -> ";
+      ret += _accessSequence[_accessNumber];
+    }
+    return ret;
   }
   void next()
   {
-    
+    if(!isFinish()){
+	    if(isPageFault()){
+
+	    }
+	  }    
   }
 
 private:
+  int _optimalIndex;
   
 };
 #endif
--- random.h
+++ random.h
@@ -2,21 +2,40 @@
 #define RANDOM_H
 
 #include "repALG.h"
+#include <cstdlib>
+#include <ctime>
 
 class Random : public RepALG
 {
 public:
-  Random(string s = ""){}
-  string victimPage() const
-  {
-    
+  Random(string s = ""){
+    srand( time(NULL) );
+  }
+  string victimPage() const{
+    string ret = "";
+    if (isPageFault()){
+      ret += to_string(_fifoIndex) + ", ";
+      if (_frameStatus.empty() || _frameStatus.size() < _fifoIndex || _frameStatus[_fifoIndex] == '\0')
+        ret += "NULL";
+      else{
+        _randomIndex = rand();
+      }
+      ret += " -> ";
+      ret += _accessSequence[_accessNumber];
+    }
+    return ret;
   }
   void next()
   {
-    
+	  if(!isFinish()){
+	    if(isPageFault()){
+
+	    }
+	  }    
   }
 
 private:
-  
+  int _fault
+  int _randomIndex;
 };
 #endif
