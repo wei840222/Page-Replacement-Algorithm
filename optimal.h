f297f1a439d4de874f78628e30e3d55116eb6adc
0penth3wind0w <0penth3wind0w@gmail.com>
2017-12-26 13:27:59 +0800

    initial commit for random and optimal

---
+++ optimal.h
@@ -0,0 +1,22 @@
+#ifndef OPTIMAL_H
+#define OPTIMAL_H
+
+#include "repALG.h"
+
+class Optimal : public RepALG
+{
+public:
+  Optimal(string s = ""){}
+  string victimPage() const
+  {
+    
+  }
+  void next()
+  {
+    
+  }
+
+private:
+  
+};
+#endif
---
+++ random.h
@@ -0,0 +1,22 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "repALG.h"
+
+class Random : public RepALG
+{
+public:
+  Random(string s = ""){}
+  string victimPage() const
+  {
+    
+  }
+  void next()
+  {
+    
+  }
+
+private:
+  
+};
+#endif
