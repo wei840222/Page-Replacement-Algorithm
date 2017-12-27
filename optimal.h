55a37feb83df7cc824b916089e1b87cb938188bf
0penth3wind0w <fc0029@yahoo.com.tw>
2017-12-28 01:13:44 +0800

    update optimal unfinished

--- optimal.h
+++ optimal.h
@@ -1,7 +1,13 @@
 #ifndef OPTIMAL_H
 #define OPTIMAL_H
 
+#include <iostream>
+
+#include <vector>
+#include <limits>
 #include "repALG.h"
+
+using namespace std;
 
 class Optimal : public RepALG
 {
@@ -12,25 +18,46 @@ public:
   {
     if(!isFinish()){
 	    if(isPageFault()){
-        if (_frameStatus.size() < _frameSize)
+        if (_frameStatus.size() < _frameSize){
           _frameStatus += _accessSequence[_accessNumber];
+          _repIndex++;
+          _repIndex %= _frameSize;
+        }
         else{
           findVictim();
-
+          _frameStatus[_repIndex] = _accessSequence[_accessNumber];
         }
 	    }
-	  }    
+	  }
+    _accessNumber++;
   }
 
 private:
   void findVictim(){
-    _repIndex = 0;
+    vector<int> _position;
     for(int i = 0; i < frameSize(); i++){
-      for(int i = _accessNumber; i < _accessSequence.size(); ++i){
-        
+      _position.push_back(infinity);
+    }
+    for(int i = 0; i < frameSize(); i++){
+      int j = _accessNumber;
+      while(j < _accessSequence.size()){
+        if(_frameStatus[i] == _accessSequence[j]){
+          _position[i] = j;
+          break;
+        }
+        ++j;
+      }
+    }
+    for(int i = 0; i < frameSize(); i++){
+      int _tmpPosition = 0;
+      if(_position[i] > _tmpPosition){
+        _tmpPosition = _position[i];
+        _repIndex = i;
       }
     }
   }
+
+  int const infinity = std::numeric_limits<int>::max();
   
 };
 #endif
