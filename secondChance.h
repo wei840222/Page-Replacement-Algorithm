15146041c14117112b0851551d142629a5ce9f42
萬俊瑋 <wei840222@outlook.com>
2017-12-28 15:09:24 +0800

    it seems like finish wait for verified

--- secondChance.h
+++ secondChance.h
@@ -1,13 +1,49 @@
 #ifndef SECONDCHANCE_H
 #define SECONDCHANCE_H
 
+#include <gtest/gtest.h>
 #include "repALG.h"
 
 class SecondChance : public RepALG
 {
-  public:
-    SecondChance(string s = "", int i = 1) : RepALG(s, i) {}
-    void next() {}
+public:
+  SecondChance(string s = "", int i = 1) : RepALG(s, i)
+  {
+    chanceTable = "";
+    for (int i = 0; i < frameSize(); i++)
+      chanceTable += '0';
+  }
+  void next()
+  {
+    if (!isFinish())
+    {
+      if (isPageFault())
+      {
+        if (_frameStatus.size() < _frameSize)
+          _frameStatus += _accessSequence[_accessNumber];
+        else
+        {
+          while (chanceTable[_repIndex] != '0')
+          {
+            chanceTable[_repIndex] = '0';
+            _repIndex++;
+            _repIndex %= _frameSize;
+          }
+          _frameStatus[_repIndex] = _accessSequence[_accessNumber];
+          _repIndex++;
+          _repIndex %= _frameSize;
+        }
+      }
+      else
+        chanceTable[_frameStatus.find(_accessSequence[_accessNumber])] = '1';
+      _accessNumber++;
+    }
+  }
+
+private:
+  string chanceTable;
+
+  FRIEND_TEST(SecondChance, chanceTable);
 };
 
 #endif
--- utSecondChance.cpp
+++ utSecondChance.cpp
@@ -3,13 +3,34 @@
 
 TEST(SecondChance, ctor)
 {
-    SecondChance *secondChance = new SecondChance("ADRSADGEEAEGSS", 3);
-    EXPECT_EQ("ADRSADGEEAEGSS", secondChance->accessSequence());
+    SecondChance *secondChance = new SecondChance("ADRASDGEEAEGSS", 3);
+    EXPECT_EQ("ADRASDGEEAEGSS", secondChance->accessSequence());
     EXPECT_EQ(3, secondChance->frameSize());
     EXPECT_EQ(0, secondChance->accessNumber());
     EXPECT_EQ("", secondChance->frameStatus());
     EXPECT_TRUE(secondChance->isPageFault());
     EXPECT_EQ("0, NULL -> A", secondChance->victimPage());
+}
+
+TEST(SecondChance, chanceTable)
+{
+    SecondChance *secondChance = new SecondChance("ADRASDGEEAEGSS", 3);
+    EXPECT_EQ("000", secondChance->chanceTable);
+    secondChance->next();
+    secondChance->next();
+    secondChance->next();
+    secondChance->next();
+    EXPECT_EQ(4, secondChance->accessNumber());
+    EXPECT_EQ("ADR", secondChance->frameStatus());
+    EXPECT_EQ("100", secondChance->chanceTable);
+    secondChance->next();
+    EXPECT_EQ(5, secondChance->accessNumber());
+    EXPECT_EQ("ASR", secondChance->frameStatus());
+    EXPECT_EQ("000", secondChance->chanceTable);
+    secondChance->next();
+    EXPECT_EQ(6, secondChance->accessNumber());
+    EXPECT_EQ("ASD", secondChance->frameStatus());
+    EXPECT_EQ("000", secondChance->chanceTable);
 }
 
 int main(int argc, char **argv)
