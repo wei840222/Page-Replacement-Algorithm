7253714e176072778a32e21e36e6664e6826c947
萬俊瑋 <wei840222@outlook.com>
2018-01-02 17:23:03 +0800

    Add continue option

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
 {
   int ALG, FRAMESIZE;
   string ACCESSSEQUENCE;
+  char CONTINUE;
   cout << "*************************************************************************************************\n"
        << "**********************                                                     **********************\n"
        << "**********************   Page Fault Replacement Algorithm Implementation   **********************\n"
@@ -21,45 +22,51 @@ int main(int argc, char **argv)
        << "*************************************************************************************************\n";
   do
   {
-    cout << "Please choose Page Replacement Algorithm\n";
-    cout << "1.FIFO 2.LRU 3.Second Chance 4.Random 5.Optimal\n";
-    cout << "Your choice:";
-    cin >> ALG;
-  } while (ALG < 1 || ALG > 5);
-  do
-  {
-    cout << "Please enter the frame size:";
-    cin >> FRAMESIZE;
-  } while (FRAMESIZE < 1);
-  cout << "Please enter the Access Sequence:";
-  cin >> ACCESSSEQUENCE;
+    do
+    {
+      cout << "Please choose Page Replacement Algorithm\n";
+      cout << "1.FIFO 2.LRU 3.Second Chance 4.Random 5.Optimal\n";
+      cout << "Your choice:";
+      cin >> ALG;
+    } while (ALG < 1 || ALG > 5);
+    do
+    {
+      cout << "Please enter the frame size:";
+      cin >> FRAMESIZE;
+    } while (FRAMESIZE < 1);
+    cout << "Please enter the Access Sequence:";
+    cin >> ACCESSSEQUENCE;
 
-  RepALG *repALG;
-  switch (ALG)
-  {
-  case 1:
-    repALG = new FIFO(ACCESSSEQUENCE, FRAMESIZE);
-    break;
-  case 2:
-    repALG = new LRU(ACCESSSEQUENCE, FRAMESIZE);
-    break;
-  case 3:
-    repALG = new SecondChance(ACCESSSEQUENCE, FRAMESIZE);
-    break;
-  case 4:
-    repALG = new Random(ACCESSSEQUENCE, FRAMESIZE);
-    break;
-  case 5:
-    repALG = new Optimal(ACCESSSEQUENCE, FRAMESIZE);
-    break;
-  }
+    RepALG *repALG;
+    switch (ALG)
+    {
+    case 1:
+      repALG = new FIFO(ACCESSSEQUENCE, FRAMESIZE);
+      break;
+    case 2:
+      repALG = new LRU(ACCESSSEQUENCE, FRAMESIZE);
+      break;
+    case 3:
+      repALG = new SecondChance(ACCESSSEQUENCE, FRAMESIZE);
+      break;
+    case 4:
+      repALG = new Random(ACCESSSEQUENCE, FRAMESIZE);
+      break;
+    case 5:
+      repALG = new Optimal(ACCESSSEQUENCE, FRAMESIZE);
+      break;
+    }
 
-  int pageFaultCount = 0;
-  cout << "\nNo\tFrame status\tVictimPage\n";
-  while (!(repALG->isFinish()))
-  {
-    cout << repALG->accessNumber() << "\t" << repALG->frameStatus() << "\t\t" << repALG->victimPage() << "\n";
-    repALG->next();
-  }
+    int pageFaultCount = 0;
+    cout << "\nNo\tFrame status\tVictimPage\n";
+    while (!(repALG->isFinish()))
+    {
+      cout << repALG->accessNumber() << "\t" << repALG->frameStatus() << "\t\t" << repALG->victimPage() << "\n";
+      repALG->next();
+    }
+    cout << "Do you want to enter next test case?(y/n)";
+    cin >> CONTINUE;
+    cout << CONTINUE;
+  } while (CONTINUE == 'Y' || CONTINUE == 'y');
   return 0;
 }
